46e563ff78c6070e9e2a629a795a86805c7b14a3
thebioengineer <ellishughes@live.com>
2019-03-10 13:51:40 -0700

    fix sha

--- js/twidget.js
+++ js/twidget.js
@@ -19,19 +19,11 @@ function getCDN(url){
 	var ghUrl = url.replace(/^(https?):\/\/(raw.)?github(?:usercontent)?\.com\/([^\/]+\/[^\/]+\/[^\/]+|[0-9A-Za-z-]+\/[0-9a-f]+\/raw)\/(.+)/i,'$1://raw.githack.com/$3/$4');
 	var matches = ghUrl.match(/^(\w+:\/\/(raw).githack.com\/([^\/]+)\/([^\/]+))\/([^\/]+)\/([^\/]+)\/(.*)/i);
 	let apiUrl = `https://api.github.com/repos/${matches[3]}/${matches[4]}/git/refs/heads/${matches[6]}`;
-	var sha = getSHA(apiUrl,matches[7]);
+	fetch(apiUrl).then(res => { if (res.ok) return res.json(); }).then(data => {
+              sha = data.object.sha ;
+            });
 	var cdnurl = cdnize(`${matches[1]}/${sha}/${matches[7]}`);
 	return cdnurl;
-}
-
-function getSHA(apiURL,match){
-	var sha = '';
-	fetch(apiURL)
-            .then(res => { if (res.ok) return res.json(); })
-            .then(data => {
-              sha = data && data.object && data.object.sha ? data.object.sha : match;
-            });
-	return sha;
 }
 
 function cdnize(url) {
