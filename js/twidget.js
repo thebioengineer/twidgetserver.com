7299978d6949dc42006b4aad1b6c6ec210e9cbd8
thebioengineer <ellishughes@live.com>
2019-03-10 17:23:24 -0700

    should work now!

--- js/twidget.js
+++ js/twidget.js
@@ -1,6 +1,5 @@
 function twidgetServerLoad() {
 	var cdn_url = getCDNurl()
-	alert(cdn_url);
 }
 
 function getCDNurl(){
@@ -19,16 +18,49 @@ function getAllUrlParams(url) {
 }
 
 function getCDN(url){
+	cdnurl = '';
 	var ghUrl = url.replace(/^(https?):\/\/(raw.)?github(?:usercontent)?\.com\/([^\/]+\/[^\/]+\/[^\/]+|[0-9A-Za-z-]+\/[0-9a-f]+\/raw)\/(.+)/i,'$1://raw.githack.com/$3/$4');
 	var matches = ghUrl.match(/^(\w+:\/\/(raw).githack.com\/([^\/]+)\/([^\/]+))\/([^\/]+)\/([^\/]+)\/(.*)/i);
 	let apiUrl = `https://api.github.com/repos/${matches[3]}/${matches[4]}/git/refs/heads/${matches[6]}`;
 	fetch(apiUrl).then(res => { if (res.ok) return res.json(); }).then(data => {
               sha = data.object.sha ;
+			  cdnurl = cdnize(`${matches[1]}/${sha}/${matches[7]}`);
+			  updatePage(cdnurl);
             });
-	var cdnurl = cdnize(`${matches[1]}/${sha}/${matches[7]}`);
-	return cdnurl;
 }
 
 function cdnize(url) {
     return url.replace(/^(\w+):\/\/(\w+)/, "$1://$2cdn");
+}
+
+function updatePage(url){
+    createIframe(url);
+	updateMetaTags
+	alert(url)
+}
+
+function createIframe(url){
+	var node = document.createElement("iframe");                 // Create an <iframe> node
+	node.setAttribute("src", url);
+	node.setAttribute("class", "widgetContainer");
+
+	document.getElementById("iframe-container").appendChild(node);     // Append <iframe> to <div> with id="iframe-container"
+}
+
+function updateMetaTags(url){
+	tags$meta(name="twitter:card",content="player"),
+	tags$meta(name="twitter:title", content="Twidget served by twidgetserver.com"),
+	tags$meta(name="twitter:description", content="Click start the Twidget"),
+	tags$meta(name="twitter:player", content=url),
+	tags$meta(name="twitter:player:width", content="600"),
+	tags$meta(name="twitter:player:height", content="450"),
+	tags$meta(name="twitter:image", content="http://www.edu.uwo.ca/img/click_to_play.png")		
+}
+
+function createMetaTag(property,value){
+	var node = document.createElement("meta");
+	node.setAttribute("name",property);
+	node.setAttribute("content",value);
+	
+	document.getElementsByTagName('head')[0].appendChild( tmpnode ); // Append <meta> to <head>
 }
